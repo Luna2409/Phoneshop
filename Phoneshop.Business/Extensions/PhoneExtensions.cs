260dba5126f13a7005db00d8bfa3ed2444e5acec
Luna Herder <luna@herderfactory.nl>
2021-12-09 11:06:58 +0100

    Added extensions for DataRow

--- Phoneshop.Business/Extensions/PhoneExtensions.cs
+++ Phoneshop.Business/Extensions/PhoneExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using Phoneshop.Domain.Objects;
 using System;
+using System.Data;
 
 namespace Phoneshop.Business.Extensions
 {
@@ -10,6 +11,21 @@ namespace Phoneshop.Business.Extensions
             double priceWithoutTax = value.PriceWithTax / 1.21;
 
             return Math.Round(priceWithoutTax, 2);
+        }
+
+        public static int GetInt(this DataRow dataRow, string columnName)
+        {
+            return Convert.ToInt32(dataRow[columnName]);
+        }
+
+        public static string GetString(this DataRow dataRow, string columnName)
+        {
+            return Convert.ToString(dataRow[columnName]);
+        }
+
+        public static double GetDouble(this DataRow dataRow, string columnName)
+        {
+            return Convert.ToDouble(dataRow[columnName]);
         }
     }
 }
